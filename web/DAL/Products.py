1ebbb61962bdf916f01cc52a2e87833721bcdb46
Erickvami <erick.vargas.minguela@gmail.com>
2020-06-04 20:09:08 -0700

    test25

--- web/DAL/Products.py
+++ web/DAL/Products.py
@@ -15,8 +15,8 @@ class Products():
     
     def getProductByName(self,product_name):
         products= list(db.product.find({"name":product_name},{'_id': False}).limit(1))
-        return {'products':products}
-        # return {'products':products,'message':'found '+ str(products.count + ' matches'}
+        # return {'products':products}
+        return {'products':products,'message':'found '+ str(products.count + ' matches'}
     
     def addProduct(self,newProduct):
         try:
