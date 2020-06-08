4a1006e27a233529e7a53fc7894a7201c5d1758c
Erickvami <erick.vargas.minguela@gmail.com>
2020-06-08 13:41:23 -0800

    get members redis type

--- web/Controllers/RedisController.py
+++ web/Controllers/RedisController.py
@@ -14,7 +14,7 @@ def addToQueue(message):
 class RedisController():
     class RedisDefault(Resource):
         def get(self):
-            x= str(list(r.smembers('messages')))
+            x= type(list(r.smembers('messages'))[0])
             return jsonify({"messages":x})
         def post(self):
             result = addToQueue(request.json["message"])#q.enqueue(addToQueue,request.json["message"])
